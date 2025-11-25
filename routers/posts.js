ccc023e4de51fa6c490c16c2eaf7216357606b23
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-25 12:44:10 +0100

    create posts route and a new file for the array

---
+++ routers/posts.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const posts = require('../posts')
+
+router.get('/', (req, res) => {
+  res.send('View all posts')
+})
+
+router.get('/:id', (req, res) => {
+  res.send(`View single post with the id: ${req.params.id}`)
+})
+
+router.post('/', (req, res) => {
+  res.send('Store a new post into the array')
+})
+
+router.put('/:id', (req, res) => {
+  res.send(`Update the element with id:${req.params.id}`)
+})
+
+router.patch('/:id', (req, res) => {
+  res.send(`Modify the element with id:${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+  res.send(`Destroy the element with id:${req.params.id}`)
+})
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const postsRouter = require('./routers/posts')
 
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
+
+app.get('/', (req, res) => {
+    res.send("This is the entrypoint of the server")
+})
+
+app.use('/api/posts', postsRouter)
